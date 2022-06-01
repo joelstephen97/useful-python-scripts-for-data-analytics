a7168944a2bfc01149bdae01d1678915094341ef
Joel Stephen <jojostev@gmail.com>
2022-06-01 22:14:56 +0400

    Create read_from_two_csv_and_write_into_third.py

---
+++ read_from_two_csv_and_write_into_third.py
@@ -0,0 +1,35 @@
+import csv
+
+
+with open('airtable-all.csv', mode='r') as f1:
+    with open('2018-2021 - Sheet1.csv', mode='r') as f2:
+        #f = open('2018-2021-updated.csv', 'w')
+        reader1 = list(csv.reader(f1))
+        reader2 = list(csv.reader(f2))
+        print(len(reader1))
+        print(len(reader2))
+        count = 0
+
+        for i in reader2:
+            if (i[5] != ''):
+                for j in reader1:
+                    if(i[5] == j[0]):
+                        count +=1
+                        #data=j[1]+":"+i[1]+":"+i[2]+":"+i[3]+":"+i[4]+":"+i[5]+":"+i[6]+":"+\
+                        #    j[7]+","+j[8] + ":" +j[2]+":"+i[9]+":"+i[10]+":"+i[11]+":"+i[12]+"\n"
+                        #print(data)
+                        #f.write(data)
+
+                    else:
+                        pass
+
+
+            else:
+                count += 1
+                #data = i[0] + ":" + i[1] + ":" + i[2] + ":" + i[3] + ":" + i[4] + ":" + i[5] + ":" + i[6] + ":" + \
+                #       i[7] + ":" + i[8] + ":" + i[9] + ":" + i[10] + ":" + i[11] + ":" + i[12]+"\n"
+                #print(data)
+                #f.write(data)
+
+        #f.close()
+        print(count)
